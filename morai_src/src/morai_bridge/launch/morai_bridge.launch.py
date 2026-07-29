0886682ab244dbbcd08951abd469ce5622095756
JunHyukIEE <huck0214@chosun.kr>
2026-07-29 16:11:00 +0900

    feat: launch all bridge and camera nodes

--- morai_src/src/morai_bridge/launch/morai_bridge.launch.py
+++ morai_src/src/morai_bridge/launch/morai_bridge.launch.py
@@ -2,12 +2,19 @@ import os
 
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch_ros.actions import Node
 
 
 def generate_launch_description():
     params = os.path.join(
         get_package_share_directory("morai_bridge"), "config", "morai_bridge.yaml"
+    )
+    camera_launch = os.path.join(
+        get_package_share_directory("udp_camera_receiver"),
+        "launch",
+        "udp_camera_receiver.launch.py",
     )
     return LaunchDescription(
         [
@@ -23,6 +30,19 @@ def generate_launch_description():
                 executable="navsat_to_gnss_node",
                 output="screen",
                 remappings=[("/gps/fix", "/sensing/gnss/ublox/nav_sat_fix")],
+            ),
+            Node(
+                package="morai_bridge",
+                executable="morai_receiver_node",
+                output="screen",
+            ),
+            Node(
+                package="morai_bridge",
+                executable="morai_sender_node",
+                output="screen",
+            ),
+            IncludeLaunchDescription(
+                PythonLaunchDescriptionSource(camera_launch),
             ),
         ]
     )
